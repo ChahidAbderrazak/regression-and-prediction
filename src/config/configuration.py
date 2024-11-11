97f0929e298e2bfe47daf774d8d6d76dedd5a55a
Abderrazak Chahid <abderrazak.chahid@gmail.com>
2024-11-11 13:41:15 -0500

    Update configuration.py

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -97,7 +97,7 @@ class ConfigurationManager:
             all_params=params,
             metric_file_name=config.metric_file_name,
             target_column=schema.name,
-            mlflow_uri="localhost:5000",
+            mlflow_uri="https://dagshub.com/ChahidAbderrazak/mlflow-zak",
         )
 
         return model_evaluation_config
